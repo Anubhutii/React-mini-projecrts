15596fc3965664592504b41973b0b5ff50e6dca7
Anubhuti Singh <anubhutis2515@gmail.com>
2026-08-05 03:23:38 +0530

    Now Finally completed

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
@@ -10,8 +10,7 @@ import Experience from "./pages/Experience";
 import Contact from "./pages/Contact";
 
 const App = () => {
-  const location = useLocation();
-
+  
   return (
     <>
       <Navbar />
